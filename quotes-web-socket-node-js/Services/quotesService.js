c1b8f5dff56432b240e00539b6ae1ae4debad5c4
Vadim Pyadukhov <vpyadukhov@outlook.com>
2019-01-03 17:58:07 +0300

    add file parser

--- quotes-web-socket-node-js/Services/quotesService.js
+++ quotes-web-socket-node-js/Services/quotesService.js
@@ -1,7 +1,22 @@
 ﻿//'use strict'
 
 var fs = require('fs');
-var fuck = require('../Data/quotes.json');
+
+const IMAGE_PATH = './Assets/Img/Quotes';
+const QUOTES_JSON_PATH = '../Data/quotes.json'
+
+var quotes = require(QUOTES_JSON_PATH);
+var images = fs.readdirSync(IMAGE_PATH);
+
+var quotesLength = quotes.length;
+var imagesLength = images.length;
+
+function randIndexInRange(max) {
+    return Math.floor(Math.random() * (max));
+}
+
+console.log(images[3]);
+//console.log(quotes[randIndexInRange(quotes.length)].text);
 
 function getImg() { }
 
